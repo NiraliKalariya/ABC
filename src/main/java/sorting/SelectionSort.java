bcab2cf01c47588700f156af97d197c1d2f1ad99
Nirali Kalariya <ndkalariya@gmail.com>
2018-08-25 23:36:58 +0530

    Added SelectionSort

--- src/main/java/sorting/SelectionSort.java
+++ src/main/java/sorting/SelectionSort.java
@@ -1,4 +1,41 @@
 package sorting;
 
 public class SelectionSort {
+    private static void exch(Comparable[] a, int i, int j) {
+        Comparable temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    public static void sort(Comparable a[]) {
+
+        for (int i = 0; i < a.length; i++) {
+            int min = i;
+            for (int j = i + 1; j < a.length; j++) {
+                if (less(a[j], a[min])) {
+                    min = j;
+                }
+            }
+            exch(a, i, min);
+        }
+    }
+
+    public static void main(String[] args) {
+        Integer[] v = {10, 5, 6, 90, 7};
+        SelectionSort.sort(v);
+        for (int i = 0; i < v.length; i++) {
+            System.out.println(v[i]);
+        }
+
+        String a[] = {"Peter", "Raj", "Alex", "Alax"};
+        SelectionSort.sort(a);
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+
+    }
 }
